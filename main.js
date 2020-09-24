1e594fe072c4b492e9511bc415004dbcc9965eb1
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2020-09-24 08:38:58 +0530

    Added Files via Visual Studio Code

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ function draw(){
    circle(590,50,90);
    circle(590,430,90);
    circle(50,430,90);
-   fill(0, 136, 0)
-   stroke(0, 136, 0)
+   fill(0, 136, 0);
+   stroke(0, 136, 0);
    rect(89, 23, 462, 55);
    rect(89, 402, 462, 55);
    rect(23, 88, 55, 304);
    rect(563, 88, 55, 304);
 }
 function take_snapshot(){
-    save("Pic.png")
+    save("Pic.png");
 }
